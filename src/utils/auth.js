b342846ac8dadc71ff21d3c616d1827a9b9e3acf
Rizky Nurfauzi <kridwan95@gmail.com>
2022-12-02 01:25:12 +0700

    Config Axios & AxiosInstance

---
+++ src/utils/auth.js
@@ -0,0 +1,24 @@
+import Cookies from "js-cookie";
+
+const Auth = {
+  isAuthorization() {
+    if (Cookies.get("token") || Cookies.get("rt")) return true;
+    return null;
+  },
+  getAccessToken() {
+    return Cookies.get("token");
+  },
+  getRefreshToken() {
+    return Cookies.get("rt");
+  },
+  getUserId() {
+    return Cookies.get("sub");
+  },
+  signOut(navigate) {
+    Cookies.remove("token");
+    Cookies.remove("rt");
+    navigate("/login");
+  },
+};
+
+export default Auth;
